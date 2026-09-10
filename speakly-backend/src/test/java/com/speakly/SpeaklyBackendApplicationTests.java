3a43831302f81f5b7f87d4a3148fb6c43868ddd7
Hugo Benitez <hugo.boca16@gmail.com>
2026-09-10 18:36:10 -0300

    swagger url

--- speakly-backend/src/test/java/com/speakly/SpeaklyBackendApplicationTests.java
+++ speakly-backend/src/test/java/com/speakly/SpeaklyBackendApplicationTests.java
@@ -1,8 +1,14 @@
 package com.speakly;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+@OpenAPIDefinition(servers = {
+		@Server(url = "/", description = "Servidor Principal (HTTPS / Local)")
+})
 @SpringBootTest
 class SpeaklyBackendApplicationTests {
 
